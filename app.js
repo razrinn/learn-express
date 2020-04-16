4bc66f5e548822a0ec074134b708681f9687ead3
Ray Azrin Karim <rayazrink@gmail.com>
2020-04-16 13:11:30 +0700

    Use MongoDB for Database

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv/config");
+
 const app = express();
 
-// Routes
+// App middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
 
-app.get("/", function (req, res) {
-    res.send("Hello World!!!!!!!!!!!!!!!!!!!!");
+// App routes
+const baseApiUrl = "/api/v1";
+app.use(baseApiUrl + "/posts", require("./routes/posts"));
+
+// Database
+mongoose.connect(
+    process.env.DATABASE_URL,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+        console.log("Connected to database...");
+    }
+);
+
+// Listen port
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
 });
-
-// Listen
-
-const port = process.env.PORT || 3000;
-
-app.listen(port);
-
-console.log("Listening on localhost:" + port);
---
+++ routes/posts.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const Post = require("../models/Post");
+
+const router = express.Router();
+
+/**
+ * GET ALL Posts
+ */
+router.get("/", async (req, res) => {
+    try {
+        const posts = await Post.find();
+        res.json(posts);
+    } catch (err) {
+        res.status(403).json(err);
+    }
+});
+
+/**
+ * GET a Post
+ */
+router.get("/:postId", async (req, res) => {
+    const postId = req.params.postId;
+    try {
+        const post = await Post.findById(postId);
+        res.json(post);
+    } catch (err) {
+        res.status(403).json(err);
+    }
+});
+
+/**
+ * CREATE a Post
+ */
+router.post("/", async (req, res) => {
+    const { title, description } = req.body;
+    try {
+        const post = await Post.create({ title, description });
+        res.json(post);
+    } catch (err) {
+        res.status(403).json(err);
+    }
+});
+
+/**
+ * DELETE a Post
+ */
+router.delete("/:postId", async (req, res) => {
+    const postId = req.params.postId;
+    try {
+        const removedPost = await Post.remove({ _id: postId });
+        res.json(removedPost);
+    } catch (err) {
+        res.status(403).json(err);
+    }
+});
+
+/**
+ * UPDATE a Post
+ */
+router.patch("/:postId", async (req, res) => {
+    const postId = req.params.postId;
+    const newPostData = req.body;
+    try {
+        const updatedPost = await Post.updateOne(
+            { _id: postId },
+            {
+                $set: newPostData,
+            }
+        );
+        res.json(updatedPost);
+    } catch (err) {}
+});
+
+module.exports = router;
